6620d04944eafb2f22f734dfa38b4ec214b516de
ollanga <ollangamoubambadavid@gmail.com>
2019-06-18 10:08:49 +0200

    first commit

---
+++ Slim0619-master/public/index.php
@@ -0,0 +1,18 @@
+<?php
+// Récupération de l'autoloader créé par composer
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+// Les "use" des différentes classes
+use Slim\App;
+
+// Config
+$config = require dirname(__DIR__) . '/config/config.php';
+
+// On créé l'application Slim
+$app = new App($config);
+
+require dirname(__DIR__) . '/config/routes.php';
+
+// Renvoi de la réponse au navigateur
+$app->run();
+
